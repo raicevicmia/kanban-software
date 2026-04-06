85e7d2821b060506103707dd684ec82eb221c720
Mia Raicevic <miarai@mailbox.unideb.hu>
2026-04-06 13:44:26 +0200

    feat: show error below column input when name is empty; prevent duplicate error messages

--- js/main.js
+++ js/main.js
@@ -6,11 +6,39 @@ render(state);
 
 addColFormEl.addEventListener("submit", (e) => {
   e.preventDefault();
-  addCol(addColInputEl.value.trim());
+
+  const colName = addColInputEl.value.trim();
+
+  if (!colName) {
+    showError();
+    setTimeout(() => {
+      clearError();
+    }, 3000);
+    return;
+  }
+
+  addCol(colName);
   render(state);
+
   addColInputEl.value = "";
 });
 
+function showError() {
+  const error = addColFormEl.querySelector(".error");
+  if (error) return;
+  else {
+    const error = document.createElement("p");
+    error.classList.add("error");
+    error.innerText = "Unesite ime kolone!";
+    addColFormEl.appendChild(error);
+    return;
+  }
+}
+
+function clearError() {
+  const error = addColFormEl.querySelector(".error");
+  if (error) error.remove();
+}
+
 //localStorage.clear();
 console.log(state);
- 
