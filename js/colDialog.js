883c192de96911bd2932240ea40d99f100540dc5
Mia Raicevic <miaraicevicc@gmail.com>
2026-04-20 15:23:18 +0200

    feat: add changing name of the list when clicked on saved button in the popup

--- js/colDialog.js
+++ js/colDialog.js
@@ -1,16 +1,25 @@
-import { saveState } from "./api.js";
+import { saveState, state } from "./api.js";
 import { renderColContainer } from "./dom.js";
 
 const dialog = document.getElementById("col-dialog");
 const color = document.getElementById("col-palette");
+const input = document.getElementById("change-col-name");
+const saveBtn = document.querySelector(".saveCol");
 
+let columnId = "";
 
 export function editCol(col, el){
   dialog.showModal();
   positionDialogRelativeTo(el, dialog);
+
+  columnId = col.id;
   
   color.onclick = null;
   color.onclick = (e) => changeColClr(e, col);
+}
+
+export function closeColDialog(){
+  dialog.close();
 }
 
 export function positionDialogRelativeTo(el, dialog) {
@@ -50,4 +59,18 @@ export function changeColClr(e, col){
   saveState();
 
   renderColContainer();
+  closeColDialog();
 }
+
+export function changeColTitle(){
+  const newTitle = input.value;
+
+  state.columns.find(col => col.id === columnId).title = newTitle;
+  saveState();
+
+  renderColContainer();
+  closeColDialog();
+}
+
+// EVENTS:
+saveBtn.addEventListener("click", changeColTitle);
